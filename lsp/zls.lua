194eba31073b454ff044d9fe82bac20e6924a84c
Dylan Meeks <dylan.meeks@aol.com>
2025-04-06 18:44:54 +0800

    zls settings update

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -1,7 +1,18 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { vim.fn.stdpath('data') .. '/mason/bin/' .. 'zls' },
-  filetypes = { 'zig', 'zir' },
-  root_markers = { 'zls.json', 'build.zig', '.git' },
-  settings = {}
+	cmd = { vim.fn.stdpath("data") .. "/mason/bin/" .. "zls" },
+	filetypes = { "zig", "zir" },
+	root_markers = { "zls.json", "build.zig", ".git" },
+	settings = {
+		zls = {
+			-- Whether to enable build-on-save diagnostics
+			--
+			-- Further information about build-on save:
+			-- https://zigtools.org/zls/guides/build-on-save/
+			enable_build_on_save = true,
+
+			-- Neovim already provides basic syntax highlighting
+			semantic_tokens = "partial",
+		},
+	},
 }
