f409f65c13744c491ed8f4208e195359dd538573
Dylan Meeks <dylan.meeks@aol.com>
2025-06-12 14:33:23 -0500

    chore: i will probably never learn lisp so ...

--- lua/plugins/plugins/fennel.lua
+++ lua/plugins/plugins/fennel.lua
@@ -4,4 +4,15 @@ return {
 	--  	version = "v2.9",
     -- },
     -- or this could be useful https://github.com/gpanders/nvim-moonwalk
+	-- { "eraserhd/parinfer-rust", build = "cargo build --release" },
+	-- {
+	-- 	"gpanders/nvim-parinfer",
+	-- 	config = function()
+	-- 		vim.g.parinfer_filetypes = {
+	-- 			"dune",
+	-- 			"scheme",
+	-- 			"query",
+	-- 		}
+	-- 	end,
+	-- },
 }
