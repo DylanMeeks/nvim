425cb9c54b890f455c8311c55c85cde0e2d7c720
Dylan Meeks <dylan.meeks@aol.com>
2025-07-08 17:36:07 -0500

    feat: add oil, its Tsoding's fault, I like editable file buffers

--- lua/plugins/plugins/oil.lua
+++ lua/plugins/plugins/oil.lua
@@ -22,6 +22,80 @@ return {
 		lazy = false,
 		config = function()
 			local detail = false
+
+			-- helper function to parse output
+			local function parse_output(proc)
+				local result = proc:wait()
+				local ret = {}
+				if result.code == 0 then
+					for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+						-- Remove trailing slash
+						line = line:gsub("/$", "")
+						ret[line] = true
+					end
+				end
+				return ret
+			end
+
+			-- build git status cache
+			local function new_git_status()
+				return setmetatable({}, {
+					__index = function(self, key)
+						local ignore_proc = vim.system(
+							{ "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+							{
+								cwd = key,
+								text = true,
+							}
+						)
+						local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+							cwd = key,
+							text = true,
+						})
+						local ret = {
+							ignored = parse_output(ignore_proc),
+							tracked = parse_output(tracked_proc),
+						}
+
+						rawset(self, key, ret)
+						return ret
+					end,
+				})
+			end
+			local git_status = new_git_status()
+
+			-- build jj status cache
+			local function new_jj_status()
+				return setmetatable({}, {
+					__index = function(self, key)
+						local ignore_proc = vim.system({}, {
+							cwd = key,
+							text = true,
+						})
+						local tracked_proc = vim.system({ "jj", "file", "list", "--quiet"}, {
+							cwd = key,
+							text = true,
+						})
+						local ret = {
+							ignored = parse_output(ignore_proc),
+							tracked = parse_output(tracked_proc),
+						}
+
+						rawset(self, key, ret)
+						return ret
+					end,
+				})
+			end
+			local jj_status = new_jj_status()
+
+			-- Clear git status cache on refresh
+			local refresh = require("oil.actions").refresh
+			local orig_refresh = refresh.callback
+			refresh.callback = function(...)
+				git_status = new_git_status()
+				orig_refresh(...)
+			end
+
 			require("oil").setup({
 				keymaps = {
 					["gd"] = {
@@ -39,7 +113,25 @@ return {
 				win_options = {
 					winbar = "%!v:lua.get_oil_winbar()",
 				},
+				view_options = {
+					is_hidden_file = function(name, bufnr)
+						local dir = require("oil").get_current_dir(bufnr)
+						local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+						-- if no local directory (e.g. for ssh connections), just hide dotfiles
+						if not dir then
+							return is_dotfile
+						end
+						-- dotfiles are considered hidden unless tracked
+						if is_dotfile then
+							return not git_status[dir].tracked[name]
+						else
+							-- Check if file is gitignored
+							return git_status[dir].ignored[name]
+						end
+					end,
+				},
 			})
+			vim.keymap.set("n", "<leader>pv", ":Oil<CR>")
 		end,
 	},
 	{
