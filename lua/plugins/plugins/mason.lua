4eaabd5a767fc8b3185476ad09b34a5456185faa
Dylan Meeks <dylan.meeks@aol.com>
2025-08-25 14:08:00 -0500

    feat: reformat meson, add some more lsps

--- lua/plugins/plugins/mason.lua
+++ lua/plugins/plugins/mason.lua
@@ -1,3 +1,18 @@
+-- Run the first available formatter followed by more formatters
+---@param bufnr integer
+---@param ... string
+---@return string
+local function first(bufnr, ...)
+	local conform = require("conform")
+	for i = 1, select("#", ...) do
+		local formatter = select(i, ...)
+		if conform.get_formatter_info(formatter, bufnr).available then
+			return formatter
+		end
+	end
+	return select(1, ...)
+end
+
 return {
 	{
 		"williamboman/mason.nvim",
@@ -33,41 +48,51 @@ return {
 				"marksman",
 				"cbfmt",
 				"taplo",
+				"tombi",
+				"gersemi",
+				"emmylua_ls",
+				"emmylua-codeformat",
+				"latexindent",
+				"tex-fmt",
+				"yq",
+				"doctoc",
 			}
 
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-			-- Run the first available formatter followed by more formatters
-			---@param bufnr integer
-			---@param ... string
-			---@return string
-			local function first(bufnr, ...)
-				local conform = require("conform")
-				for i = 1, select("#", ...) do
-					local formatter = select(i, ...)
-					if conform.get_formatter_info(formatter, bufnr).available then
-						return formatter
-					end
-				end
-				return select(1, ...)
-			end
-
-			local conform = require("conform")
-			conform.setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					sh = { "beautysh" },
-					zig = { "zigfmt" },
-					ocaml = { "ocamlformat" },
-					markdown = function(bufnr)
-						return { first(bufnr, "marksman", "cbfmt") }
-					end,
-					python = { "ruff", "black" },
-					go = { "goimports", "gofmt" },
-					c = { "clang-format" },
-                    cpp = { "clang-format" },
-				},
-			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		---@module "conform"
+		---@type conform.setupOpts
+		opts = {
+			formatters_by_ft = {
+				c = { "clang-format" },
+				cmake = { "gersemi" },
+				cpp = { "clang-format" },
+				go = function(bufnr)
+					return { first(bufnr, "goimports", "gofmt") }
+				end,
+				lua = { "stylua" },
+				markdown = function(bufnr)
+					return { first(bufnr, "marksman", "cbfmt") }
+				end,
+				ocaml = { "ocamlformat" },
+				python = { "ruff", "black" },
+				sh = { "beautysh" },
+				toml = { "tombi" },
+				yaml = { "yq" },
+				tex = { "latexindent", "tex-fmt" },
+				zig = { "zigfmt" },
+			},
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+		},
+		init = function()
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 		end,
 	},
 }
