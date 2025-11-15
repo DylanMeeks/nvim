4bda00bb5eb05375289c8c5d796e68a3596a374f
Dylan Meeks <dylan.meeks@aol.com>
2025-11-14 20:54:55 -0600

    chore: tabs to spaces

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -1,30 +1,30 @@
 ---@type vim.lsp.Config
 return {
-	cmd = { vim.fn.stdpath("data") .. "/mason/bin/" .. "gopls" },
-	filetypes = { "go", "gomod", "gosum" },
-	root_markers = {
-		"go.mod",
-		"go.sum",
-	},
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
-				fieldalignment = true,
-				inferTypeArgs = true,
-			},
-			hints = {
-				assignVariableTypes = true,
-				compositeLiteralFields = true,
-				compositeLiteralTypes = true,
-				constantValues = true,
-				functionTypeParameters = true,
-				parameterNames = true,
-				rangeVariableTypes = true,
-			},
-			staticcheck = true,
-			gofumpt = true,
-			semanticTokens = true,
-		},
-	},
+    cmd = { vim.fn.stdpath("data") .. "/mason/bin/" .. "gopls" },
+    filetypes = { "go", "gomod", "gosum" },
+    root_markers = {
+        "go.mod",
+        "go.sum",
+    },
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+                fieldalignment = true,
+                inferTypeArgs = true,
+            },
+            hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+            semanticTokens = true,
+        },
+    },
 }
