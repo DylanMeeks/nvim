0aa85ec0a2f04e3524462a0b0f17553619dcb605
Dylan Meeks <dylan.meeks@aol.com>
2025-12-23 19:00:32 -0600

    feat: lsp stuff

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -1,18 +1,19 @@
 ---@type vim.lsp.Config
 return {
-	cmd = { vim.fn.stdpath("data") .. "/mason/bin/" .. "zls" },
-	filetypes = { "zig", "zir" },
-	root_markers = { "zls.json", "build.zig", ".git" },
-	settings = {
-		zls = {
-			-- Whether to enable build-on-save diagnostics
-			--
-			-- Further information about build-on save:
-			-- https://zigtools.org/zls/guides/build-on-save/
-			enable_build_on_save = true,
+    -- cmd = { vim.fn.stdpath("data") .. "/mason/bin/" .. "zls" },
+    cmd = { "zls" },
+    filetypes = { "zig", "zir" },
+    root_markers = { "zls.json", "build.zig", ".git" },
+    settings = {
+        zls = {
+            -- Whether to enable build-on-save diagnostics
+            --
+            -- Further information about build-on save:
+            -- https://zigtools.org/zls/guides/build-on-save/
+            enable_build_on_save = true,
 
-			-- Neovim already provides basic syntax highlighting
-			semantic_tokens = "partial",
-		},
-	},
+            -- Neovim already provides basic syntax highlighting
+            semantic_tokens = "partial",
+        },
+    },
 }
